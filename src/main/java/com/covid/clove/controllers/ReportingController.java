bd67626ee2187db83cd7b55aee9bb1dd2dcff047
bhuvanakri <bhuvanakri@gmail.com>
2020-07-16 00:28:59 +0530

    chore: patient record

--- src/main/java/com/covid/clove/controllers/ReportingController.java
+++ src/main/java/com/covid/clove/controllers/ReportingController.java
@@ -1,7 +1,9 @@
 package com.covid.clove.controllers;
 
-import com.covid.clove.health.CreateDataSetImpl;
-import com.covid.clove.health.GetDataSetImpl;
+import com.covid.clove.health.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/report")
 public class ReportingController {
-    private static final String PROJECT_ID = System.getenv("PROJECT_ID");
+    private static final String PROJECT_ID = "getProjectid";
     private static final String REGION_ID = "us-central1";
     @RequestMapping(value = "/createdata")
     public ResponseEntity<Object> createaData() {
@@ -28,8 +30,8 @@ public class ReportingController {
     }
 
     @RequestMapping(value = "/getdata")
-    public ResponseEntity<Object> getdata() {
-        String datasetId = "dataset-" + UUID.randomUUID().toString().replaceAll("-", "_");
+    public ResponseEntity<Object> getData() {
+        String datasetId = "getid";
         String datasetName =
                 String.format("projects/%s/locations/%s/datasets/%s", PROJECT_ID, REGION_ID, datasetId);
         try {
@@ -39,6 +41,83 @@ public class ReportingController {
             exp.printStackTrace();
         }
         return ResponseEntity.ok().body("get success");
+    }
+
+    @RequestMapping(value = "/deidentifydata")
+    public ResponseEntity<Object> deidentifyData() {
+        String datasetId = "getid";
+        String datasetName =
+                String.format("projects/%s/locations/%s/datasets/%s", PROJECT_ID, REGION_ID, datasetId);
+        try {
+            return ResponseEntity.ok().body(DeIdentifyImpl.datasetDeIdentify(datasetName, datasetName + "-died"));
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+        return ResponseEntity.ok().body("get success");
+    }
+
+    @RequestMapping(value = "/fhircreatestore")
+    public ResponseEntity<Object> fhircreateStore() {
+        String datasetId = "getid";
+        String datasetName =
+                String.format("projects/%s/locations/%s/datasets/%s", PROJECT_ID, REGION_ID, datasetId);
+        String fhirStoreId = "my-fhir-store-1";
+        String fhirStoreName = String.format("%s/fhirStores/%s", datasetName, fhirStoreId);
+        try {
+            return ResponseEntity.ok().body(FHIRStoreCreateImpl.fhirStoreCreate(datasetName, fhirStoreId));
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+        return ResponseEntity.ok().body("get success");
+    }
+
+    @RequestMapping(value = "/fhircreateresource")
+    public ResponseEntity fhircreateResource() {
+
+        String datasetId = "getid";
+        String datasetName =
+                String.format("projects/%s/locations/%s/datasets/%s", PROJECT_ID, REGION_ID, datasetId);
+        String fhirStoreId = "my-fhir-store-1";
+        String fhirStoreName = String.format("%s/fhirStores/%s", datasetName, fhirStoreId);
+        try {
+            return ResponseEntity.ok().body(EntityUtils.toString(FHIRResourceCreateImpl.fhirResourceCreate(fhirStoreName, "Patient")));
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/fhirgetpatientrecord")
+    public ResponseEntity fhirGetPatientRecord() {
+        String FHIR_NAME =
+                "projects/%s/locations/%s/datasets/%s/fhirStores/%s/fhir/%s";
+        String datasetId = "getid";
+        String fhirStoreId = "my-fhir-store-1";
+        String fhirResourceId = "Patient/getresouceid";
+        String fhirResourceName = String.format(FHIR_NAME, PROJECT_ID, REGION_ID, datasetId, fhirStoreId, fhirResourceId);
+
+        try {
+            return ResponseEntity.ok().body(EntityUtils.toString(FHIRGetPatientRecordImpl.fhirResourceGetPatientEverything(fhirResourceName)));
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+        return null;
+    }
+    @RequestMapping(value = "/fhirgetresourcerecord")
+    public ResponseEntity fhirGetResourceRecord() {
+        String FHIR_NAME =
+                "projects/%s/locations/%s/datasets/%s/fhirStores/%s/fhir/%s";
+        String datasetId = "getid";
+        String fhirStoreId = "my-fhir-store-1";
+        String fhirResourceId = "Patient/getresouceid";
+        String fhirResourceName = String.format(FHIR_NAME, PROJECT_ID, REGION_ID, datasetId, fhirStoreId, fhirResourceId);
+
+        try {
+            return ResponseEntity.ok().body(EntityUtils.toString(FHIRGetResourceRecord.fhirResourceGet(fhirResourceName)));
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+        return null;
     }
 
 
